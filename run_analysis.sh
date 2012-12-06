ea3f48cd2ce4546bbc1c9e2c48d5e5b397e55f63
kuj1pal <kuehnjoh@gmail.com>
2012-12-06 11:30:52 -0800

    ..

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -ex
-
+/bin/echo '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ run_analysis.sh ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
 # Add ros sources to apt
 #sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $OS_PLATFORM main" > /etc/apt/sources.list.d/ros-latest.list'
 #wget http://packages.ros.org/ros.key -O $WORKSPACE/ros.key
 #apt-key add $WORKSPACE/ros.key
+cp $HOME/chroot_configs/sources.list /etc/apt/sources.list
 sudo apt-get update
 #wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
