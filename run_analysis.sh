fb0962a0f930f32a222a655d269c33738a54fd5a
kuj1pal <kuehnjoh@gmail.com>
2012-12-06 11:38:10 -0800

    ..

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 /bin/echo '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ run_analysis.sh ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
 # Add ros sources to apt
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $OS_PLATFORM main" > /etc/apt/sources.list.d/ros-latest.list'
-#wget http://packages.ros.org/ros.key -O $WORKSPACE/ros.key
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $OS_PLATFORM main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O $WORKSPACE/ros.key
 #apt-key add $WORKSPACE/ros.key
-cp $HOME/chroot_configs/sources.list /etc/apt/sources.list
+#cp $HOME/chroot_configs/sources.list /etc/apt/sources.list
 sudo apt-get update
 #wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 
@@ -12,6 +12,7 @@ sudo apt-get update
 # install stuff we need
 echo "Installing Debian packages we need for running this script"
 sudo apt-get install python-rosinstall python-rospkg python-tk ia32-libs openssh-server ros-electric-ros-base ros-electric-ros-release --yes
+rospack find 
 
 source /opt/ros/$ROS_DISTRO/setup.sh
 source $HOME/chroot_configs/set_qacpp_path.sh
